0701045f8e5598369e7b228d6a37d1c66f28aa49
YungHao <yunghao.yong@ecloudvalley.com>
2024-10-19 16:01:27 +0800

    add diagram

---
+++ main.tf
@@ -0,0 +1,21 @@
+# VPC
+
+# Public Subnet
+
+# Private Subnet
+
+# Internet Gateway
+
+# NAT Gateway
+
+# Public Route Table
+
+# Private Route Table
+
+# S3 Gateway Endpoint
+
+# EC2 Instance Endpoint
+
+# S3 Bucket
+
+# EC2 Instance
